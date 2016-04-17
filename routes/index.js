06025b7f9891216f66fe02504580ddade5621861
Pontus Magnusson <pontuspdmagnusson@gmail.com>
2016-04-17 15:43:37 +0200

    Changes missed in commit "Async added, barrier removed..."

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-var simpleBarrier = require('simple-barrier');
+var async = require('async');
 var navigation = require('./modules/navigation-module');
 var news = require('./modules/news-module.js');
 
@@ -7,8 +7,32 @@ module.exports = function(router, app){
         
         var client = app.get('client');
         
+        async.parallel({
+                news: function(callback) {
+                    news.getNews(client, callback);
+                },
+                posts:  function(callback) {
+                    news.getFacebookPagePosts(client, callback, "dalomotors");
+                },
+                picture:  function(callback) {
+                    news.getFacebookPagePicture(client, callback);
+                },
+                navigation:  function(callback) {
+                    navigation.getLoginNavigation(client, callback);
                 }
+            },
+            function(err, results) {
+                // Render page and send data
+                return response.render('index', {
+                    title: 'News - Dalo',
+                    news: results.news,
+                    fbfeed: results.posts,
+                    fbpicture: results.picture,
+                    login_nav: results.navigation
+                });
             }
+        );
     });
 
     return router;
+};
