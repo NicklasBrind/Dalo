aebb98aa1d04109baa912f65c18a8d2e1eb6203a
Pontus Magnusson <pontuspdmagnusson@gmail.com>
2016-04-28 21:17:52 +0200

    Variables created

--- routes/join.js
+++ routes/join.js
@@ -13,9 +13,46 @@ module.exports = function(router, app){
        // Get client
         var client = app.get('client');
         
+        var data = [];
+        
         var firstname = new stringvalidator.StringValidator(request.body.firstname);
         firstname.addRule(new stringvalidator.RuleMaxLength(3));
-        console.log(firstname.validate());
+        data.push(firstname);
+        
+        var lastname = new stringvalidator.StringValidator(request.body.lastname);
+        
+        var nickname = new stringvalidator.StringValidator(request.body.nickname);
+        
+        var ssn = new stringvalidator.StringValidator(request.body.ssn);
+        
+        var email = new stringvalidator.StringValidator(request.body.email);
+        
+        var password = new stringvalidator.StringValidator(request.body.password);
+        
+        var password = new stringvalidator.StringValidator(request.body.validpassword);
+        
+        var program = new stringvalidator.StringValidator(request.body.program);
+        
+        var startyear = new stringvalidator.StringValidator(request.body.startyear);
+        
+        var examyear = new stringvalidator.StringValidator(request.body.examyear);
+        
+        var street = new stringvalidator.StringValidator(request.body.street);
+        
+        var zipcode = new stringvalidator.StringValidator(request.body.zipcode);
+        
+        var city = new stringvalidator.StringValidator(request.body.city);
+        
+        var country = new stringvalidator.StringValidator(request.body.country);
+        
+        var phonenumber = new stringvalidator.StringValidator(request.body.phonenumber);
+        
+        var facebook = new stringvalidator.StringValidator(request.body.facebook);
+        
+        var linkedin = new stringvalidator.StringValidator(request.body.linkedin);
+        
+        var github = new stringvalidator.StringValidator(request.body.github);
+        
     });
     
     return router;
