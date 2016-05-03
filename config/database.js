221907a3220062d22c4b72630e65a457db0af5f0
Nicklas <brind@live.se>
2016-05-03 17:49:19 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
+// Dummy config file
 module.exports = {
     connection : {
-    host : '193.11.129.231',
-    port : '3300',
-    user : 'dalo',
-    password : 'hemsidebygge',
-    database : 'hamburger'  
+    host : 'host',
+    port : 'port',
+    user : 'user',
+    password : 'password',
+    database : 'database'  
     }
 };
