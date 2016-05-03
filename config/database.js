502b784ab3e165bec987da2294c5d2c2a91dab32
WanNickB <brind@live.se>
2016-05-03 17:48:23 +0200

    updated ignore

--- config/database.js
+++ config/database.js
@@ -1,11 +1,9 @@
 module.exports = {
     connection : {
-        host : 'host',
-        port : 'port',
-        user : 'user',
-        password : 'password',
-    },
-    database: {
-        database : 'database'    
+    host : '193.11.129.231',
+    port : '3300',
+    user : 'dalo',
+    password : 'hemsidebygge',
+    database : 'hamburger'  
     }
 };
