1d9ac4ef0a55af2271e1010e9f338ab61a296dca
WanNickB <brind@live.se>
2016-10-17 03:11:24 +0200

    now returns promise and takes a user as param in construct

--- modules/Navigation.js
+++ modules/Navigation.js
@@ -1,9 +1,29 @@
 "use strict";
+const Promise = require('bluebird');
+
 class Navigation{
-    constructor(){
+    constructor(user){
+        this.user = user || undefined
         this.menu = [];
     }
+    
+    getLoginNavigation(){
+        if(this.user){
+            // Material icons are found here: https://design.google.com/icons/
+            this.menu.push( {link: "logout", href: "/logout", materialicon: "directions_run"} );
+            this.menu.push( {link: "profile", href: "/profile", materialicon: "account_box"} );
+            this.menu.push( {link: "wiki", href: "/wiki", materialicon: "language"} );
+            if(this.user.admin){
+               this.menu.push( {link: "admin", href: "/admin", materialicon: "build"} ); 
+            }
+        }else{  
+            this.menu.push( {link: "login", href: "/login", materialicon: "exit_to_app"} );
+        }
+        return Promise.resolve(this.menu);
+    }
+    
+ 
 }
 
 
-module.exports = new Navigation();
+module.exports = Navigation;
